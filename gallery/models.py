1d10a65a078f7f603dc7fca88d8f6e54ac7b24da
karobia001 <karobiamaina81@gmail.com>
2019-11-08 17:35:50 +0300

    creating the image class that will take in our cla]]]

--- gallery/models.py
+++ gallery/models.py
@@ -36,3 +36,70 @@ class Category(models.Model):
     def get_category(cls):
         categories = cls.objects.all()
         return categories
+    
+    @classmethod
+    def search_by_category(cls, search_term):
+        images = cls.objects.filter(category__icontains=search_term)
+        return images
+    
+    def __str__(self):
+        return self.category
+   
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        
+class Image(models.Model):
+    image_name = models.CharField(max_length=255)
+    description = models.TextField()
+    image_file = models.ImageField
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
+       
+    def save_image(self):
+        self.save()
+    
+    def delete_image(self):
+        self.delete()
+        
+    @classmethod
+    def get_images(cls):
+        images = cls.objects.all()
+        return images
+    
+    @classmethod
+    def search_by_categ(cls, search_term):
+        images = cls.objects.filter(category__category__icontains=search_term)
+        return images
+    
+    @classmethod
+    def search_by_location(cls, location):
+        images = cls.objects.filter(location__location=location)
+        return images
+    
+    @classmethod
+    def get_by_category(cls, category):
+        images = cls.objects.filter(category__category=category)
+        return images
+    
+    @classmethod
+    def get_image(request, id):
+        locations = Location.get_location()
+        try:
+            image = Image.objects.get(pk = id)
+            print(image)
+            
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return image
+    
+    def __str__(self):
+        return self.image_name
+    
+    class Meta:
+        ordering = ['image_name']
+        verbose_name = 'My image'
+        verbose_name_plural = 'Images'
+   

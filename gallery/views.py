c50e960a01b28fe0ce78561a41a2d8e2c4ad80d2
karobia001 <karobiamaina81@gmail.com>
2019-11-11 17:53:44 +0300

    changes the name in our category and location

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime as dt
+from .models import *
 
 # Create your views here.
 def welcome(request):
@@ -8,22 +9,55 @@ def welcome(request):
 
 def index(request):
     date = dt.date.today()
-    images = Image.get_images()
+    images = Image.objects.all()
     location = Location.get_location()
     locations = Location.get_location()
 
-    return render(request, 'base.html', {"date": date, "images":images, "location": location, "locations": locations})
+    return render(request, 'base.html', {"date": date, "images":images, "location": location, "location_results": location})
 
 
-def search_results(request):
-
-    if 'category' in request.GET and request.GET["category"]:
-        search_term = request.GET.get("category")
-        searched_category = Category.search_by_title(search_term)
+def search_images(request):
+    locations = Location.get_location()
+    if 'keyword' in request.GET and request.GET["keyword"]:
+        search_term = request.GET.get("keyword")
+        searched_images = Image.search_by_categ(search_term)
         message = f"{search_term}"
 
-        return render(request, 'search.html',{"message":message,"articles": searched_category})
+        return render(request, 'base.html', {"message":message,"images": searched_images, "locations": locations})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'search.html',{"message":message})
+        return render(request, 'base.html', {"message": message, "locations":locations})
+
+
+def get_image(request, id):
+        locations = Location.get_location()
+        try:
+            image = Image.objects.get(pk = id)
+            print(image)
+            
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return render(request, "base.html", {"image":image, "locations":locations})
+    
+    
+def location(request, location):
+    images = Image.search_by_location(location)
+    locations = Location.get_location()
+    message = f"{location}"
+    return render(request, 'base.html', {"message":message, "images":images, "locations":locations})
+
+
+def category(request, category):
+    images = Image.get_by_category(category)
+    locations = Location.get_location()
+    message = f"{category}"
+    return render(request, 'base.html', {"message":message, "images":images, "locations":locations})
+
+
+def navlocation(request):
+    
+    locations = Location.get_location()
+
+    return render(request, 'navbar.html', {"locations": locations})
